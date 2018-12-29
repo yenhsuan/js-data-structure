712bfb0728804395714b170990791a4799a8b299
yenhsuan <yenhsuan.terry@gmail.com>
2018-12-28 22:26:09 -0800

    set up mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
-  "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "plugins": [
+    "mocha",
+  ],
+  "rules": {
+    "mocha/no-exclusive-tests": "error",
+    "no-unused-vars": [
+      "error",
+      {
+        "varsIgnorePattern": "should|expect",
+      },
+    ],
+  },
+  "env": {
+    "mocha": true,
+  }
 };
