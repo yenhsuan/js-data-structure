317ae09f8fcb9c5fa28f5a44745e947a2790e2fd
yenhsuan <yenhsuan.terry@gmail.com>
2018-12-29 23:27:10 -0800

    add test cases

--- test/data_structures/deque.test.js
+++ test/data_structures/deque.test.js
@@ -7,10 +7,10 @@ describe('# Deque', () => {
     it('should be empty after initialized', () => {
       const deque = new Deque();
       deque.size().should.be.equal(0);
-      should(deque.pop()).be.equal(null);
-      should(deque.peek()).be.equal(null);
-      should(deque.popFront()).be.equal(null);
-      should(deque.peekFront()).be.equal(null);
+      should(deque.pop()).be.Null();
+      should(deque.peek()).be.Null();
+      should(deque.popFront()).be.Null();
+      should(deque.peekFront()).be.Null();
     });
 
     it('should throw error if given a invalid capacity', () => {
@@ -40,7 +40,7 @@ describe('# Deque', () => {
         should(deque.pop()).be.deepEqual(elems[i]);
         deque.size().should.be.equal(i);
       }
-      should(deque.pop()).be.equal(null);
+      should(deque.pop()).be.Null();
     });
   });
 
@@ -64,7 +64,7 @@ describe('# Deque', () => {
         should(deque.popFront()).be.deepEqual(elems[i]);
         deque.size().should.be.equal(i);
       }
-      should(deque.popFront()).be.equal(null);
+      should(deque.popFront()).be.Null();
     });
   });
 
@@ -95,7 +95,51 @@ describe('# Deque', () => {
     });
   });
 
+  describe('clear', () => {
+    const deque = new Deque();
+    deque.push({});
+    deque.pushFront({ a: 1 });
+    deque.push('some string');
+
+    deque.clear();
+    deque.size().should.be.equal(0);
+    should(deque.peek()).be.Null();
+    should(deque.peekFront()).be.Null();
+    should(deque.isEmpty()).be.True();
+  });
+
+  describe('toString', () => {
+    it('should return empty string if deque is empty', () => {
+      const deque = new Deque();
+      deque.toString().should.be.equal('');
+    });
+
+    it('should return the same format as Array.toString()', () => {
+      const test = [1, 3, 2, 4, 5, 9, 8, 0, 7];
+      const deque = new Deque();
+      test.forEach((n) => {
+        deque.pushFront(n);
+      });
+
+      deque.toString().should.be.equal(test.reverse().toString());
+    });
+  });
+
   describe('mix push/pop/pushFront/popFront operations', () => {
+    it('should be able to push elements at both side)', () => {
+      const deque = new Deque();
+      deque.pushFront(3);
+      deque.pushFront(2);
+      deque.pushFront(1);
+      deque.push(4);
+      deque.push(5);
+      deque.push(6);
+
+      for (let i = 1; i < 6; i += 1) {
+        should(deque.popFront()).be.equal(i);
+      }
+    });
+
     it('should be able to perform queue-like operation (push/popFront)', () => {
       const deque = new Deque();
       for (let i = 0; i < 10; i += 1) {
