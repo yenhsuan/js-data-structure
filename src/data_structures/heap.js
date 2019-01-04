e72886e701d2e0c42b6fec7a466c776958513987
yenhsuan <yenhsuan.terry@gmail.com>
2019-01-04 00:54:00 -0800

    add heap implementation

--- src/data_structures/heap.js
+++ src/data_structures/heap.js
@@ -1,20 +1,79 @@
 const internal = require('../utils/private-props');
 const ERR_MSG = require('../utils/errors');
 
+/* helpers */
 const defaultComparator = (self, other) => {
   if (self === other) {
     return 0;
   }
-
   return self > other ? 1 : -1;
 };
 
+const parentIdx = (idx) => {
+  if (idx === 0) {
+    return undefined;
+  }
+  return Math.floor((idx - 1) / 2);
+};
+
+const leftChildIdx = (idx, len) => {
+  if (idx > len - 1) {
+    return undefined;
+  }
+  return idx * 2 + 1;
+};
+
+const rightChildIdx = (idx, len) => {
+  if (idx > len - 1) {
+    return undefined;
+  }
+  return idx * 2 + 2;
+};
+
+/* private methods */
 const privateMethods = {
-  siftUp: function moveElememtUpInHeap() {
+  siftUp: function moveElememtUpInHeap(idx) {
+    if (idx === 0) {
+      return;
+    }
 
+    let parent = parentIdx(idx);
+    let child = idx;
+
+    while (parent !== undefined && internal(this).cmp(child, parent) < 0) {
+      privateMethods.swap(child, parent);
+      child = parent;
+      parent = parentIdx(idx);
+    }
   },
-  siftDown: function moveElememtDownInHeap() {
+  siftDown: function moveElememtDownInHeap(idx) {
+    if (idx > internal(this).elems.length - 1) {
+      return;
+    }
 
+    let parent = idx;
+    let left = leftChildIdx(parent, internal(this).length);
+    let right = rightChildIdx(parent, internal(this).length);
+
+    while ((left !== undefined) || (right !== undefined)) {
+      if (left !== undefined && internal(this).cmp(parent, left) > 0) {
+        privateMethods.swap(parent, left).apply(this);
+        parent = left;
+      } else if (right !== undefined && internal(this).cmp(parent, right) > 0) {
+        privateMethods.swap(parent, right).apply(this);
+        parent = right;
+      } else {
+        break;
+      }
+
+      left = leftChildIdx(parent, internal(this).length);
+      right = rightChildIdx(parent, internal(this).length);
+    }
+  },
+  swap: function swapTwoNodeInHeap(selfIdx, otherIdx) {
+    const temp = internal(this).elems[selfIdx];
+    internal(this).elems[selfIdx] = internal(this).elems[otherIdx];
+    internal(this).elems[otherIdx] = temp;
   },
 };
 
